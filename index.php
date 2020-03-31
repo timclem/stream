88cfa37999465ef7ae350c3c6b7cb11a0239b76b
Favre Valentin <61196897+Valentinfavre@users.noreply.github.com>
2020-03-31 11:14:37 +0200

    prepare index non movable menu and some spritesheets

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Clevrean</title>
+  </head>
+  <link rel="stylesheet" href="./style/style.css">
+  <body>
+    <div id="bar_lateral_1">
+      <ul>
+        <img src="" id="icons_nav" alt="">
+        <li>Films</li>
+        <img src="" id="icons_nav" alt="">
+        <li>Sèries</li>
+        <img src="" id="icons_nav" alt="">
+        <li>Videos</li>
+        <img src="" id="icons_nav" alt="">
+        <li>Musique</li>
+      </ul>
+    </div>
+    <div id="bar_lateral_2">
+      <ul>
+        <img src="" id="icons_nav" alt="">
+        <li>Mon Compte</li>
+        <img src="" id="icons_nav" alt="">
+        <li>Paramétres</li>
+      </ul>
+    </div>
+  </body>
+</html>
