72defc67826f2cc4301ae095292409600e2a91f0
Favre Valentin <61196897+Valentinfavre@users.noreply.github.com>
2020-04-06 11:14:17 +0200

    evolution design

--- index.php
+++ index.php
@@ -6,23 +6,30 @@
   </head>
   <link rel="stylesheet" href="./style/style.css">
   <body>
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+
+      </ul>
+    </nav>
     <div id="bar_lateral_1">
       <ul>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/films.png" id="icons_nav" alt="">
         <li>Films</li>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/series.png" id="icons_nav" alt="">
         <li>Sèries</li>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/video.png" id="icons_nav" alt="">
         <li>Videos</li>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/music.png" id="icons_nav" alt="">
         <li>Musique</li>
       </ul>
     </div>
     <div id="bar_lateral_2">
       <ul>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/account.png" id="icons_nav" alt="">
         <li>Mon Compte</li>
-        <img src="" id="icons_nav" alt="">
+        <img src="./ressources/icon/settings.png" id="icons_nav" alt="">
         <li>Paramétres</li>
       </ul>
     </div>
