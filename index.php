d46c29be7d5c6f04ebc7f44ec7f2a2ba67b14195
Favre Valentin <61196897+Valentinfavre@users.noreply.github.com>
2020-05-08 16:06:04 +0200

    add some improuvement to start Sprint N°1

--- index.php
+++ index.php
@@ -13,18 +13,9 @@
 
       </ul>
     </nav>
-    <div id="bar_lateral_1">
-      <ul>
-        <img src="./ressources/icon/films.png" id="icons_nav" alt="">
-        <li>Films</li>
-        <img src="./ressources/icon/series.png" id="icons_nav" alt="">
-        <li>Sèries</li>
-        <img src="./ressources/icon/video.png" id="icons_nav" alt="">
-        <li>Videos</li>
-        <img src="./ressources/icon/music.png" id="icons_nav" alt="">
-        <li>Musique</li>
-      </ul>
-    </div>
+    <?php
+      include "./nav_services.php"
+      ?>
     <div id="bar_lateral_2">
       <ul>
         <img src="./ressources/icon/account.png" id="icons_nav" alt="">
---
+++ nav_services.php
@@ -0,0 +1,12 @@
+<div id="bar_lateral_1">
+  <ul>
+    <img src="./ressources/icon/films.png" id="icons_nav" alt="">
+    <li>Films</li>
+    <img src="./ressources/icon/series.png" id="icons_nav" alt="">
+    <li>Sèries</li>
+    <img src="./ressources/icon/video.png" id="icons_nav" alt="">
+    <li>Videos</li>
+    <img src="./ressources/icon/music.png" id="icons_nav" alt="">
+    <li>Musique</li>
+  </ul>
+</div>
